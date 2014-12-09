71328d5118f015c9b50ba68b6d96945da9504a9d
Andrew Rhoads <andrew.c.rhoads+github@gmail.com>
2014-12-09 15:20:08 -0600

    rename outfit column name: title -> name

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -20,6 +20,6 @@ class OutfitsController < ApplicationController
 
   private
   def outfit_params
-    params.require(:outfit).permit(:image, :title, :description, :gender)
+    params.require(:outfit).permit(:image, :name, :description, :gender)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209021316) do
+ActiveRecord::Schema.define(version: 20141209211545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20141209021316) do
     t.datetime "image_updated_at"
     t.integer  "userid",             null: false
     t.text     "description"
-    t.text     "title"
+    t.text     "name"
     t.text     "gender"
   end
 
