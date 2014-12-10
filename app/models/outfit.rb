6e3a1bcf54825315a492f6726c9eb5872885836f
Andrew Rhoads <andrew.c.rhoads+github@gmail.com>
2014-12-09 19:27:27 -0600

    Clicking on outfit image takes you to fullsize picture

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -4,8 +4,8 @@ class Outfit < ActiveRecord::Base
 
   # This method associates the attribute ":image" with a file attachment
 	# '>' specifies resize option for imagemagick
- 	has_attached_file :image, styles: { medium: "450x800>", thumb: "100x100>" }
+  has_attached_file :image, styles: { medium: "800x800>", thumb: "100x100>" }
 
  	# Validate the attached image is image/jpg, image/png, etc
- 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
