0aa533584409b3ab01ed97690c726b510c5a205e
Joshua Michael Hertlein <jmhertlein@gmail.com>
2014-12-10 21:17:27 -0600

    Added the beginnings of a live front page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,9 @@
 class LandingController < ApplicationController
 
   def index
+    @males = Outfit.order(:created_at => :desc).where(gender: "male").limit(5)
+    @females = Outfit.order(:created_at => :desc).where(gender: "female").limit(5)
 
+    @top_images = {"male" => @males, "female" => @females}
   end
 end
