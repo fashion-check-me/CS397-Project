5d3702fdf49e0885cbc78384be2b9da52536a2f2
Joshua Michael Hertlein <jmhertlein@gmail.com>
2014-12-10 22:01:57 -0600

    Added unisex column to the middle

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,7 +3,8 @@ class LandingController < ApplicationController
   def index
     @males = Outfit.order(:created_at => :desc).where(gender: "male").limit(5)
     @females = Outfit.order(:created_at => :desc).where(gender: "female").limit(5)
+    @unisex = Outfit.order(:created_at => :desc).where(gender: "unisex").limit(5)
 
-    @top_images = {"male" => @males, "female" => @females}
+    @top_images = {"male" => @males, "female" => @females, "unisex" => @unisex}
   end
 end
