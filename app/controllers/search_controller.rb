fae78607455a59ce0ad9c7695f8d8b8ca86a6a97
Joshua Michael Hertlein <jmhertlein@gmail.com>
2015-02-26 14:37:47 -0600

    search: explaining why we're copying/filtering like this

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,8 +5,9 @@ class SearchController < ApplicationController
 
   def search
     args = Hash.new
+    # search_params is not a vanilla Hash, so we need to convert it to one
     search_params.each do |k,v|
-      args[k] = v unless v.empty?
+      args[k] = v unless v.empty? # this removes both :search and any unused search params
     end
     @result = Tag.where(args).map {|t| Outfit.find(t.outfitid)}.uniq
   end
