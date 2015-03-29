baaa3bfdacdc9de1e7651d2b16d0bd8215b58f7d
Joshua M Hertlein <jmhertlein@gmail.com>
2015-03-28 20:43:20 -0500

    Added barebones users/show.html, "My Profile" in title bar works now
    Work on #10
    Added path helper user_profile_path(username) will return URL for users/$username/

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,7 @@ class UsersController < ApplicationController
 
   def show
     flash.alert = "DEBUG: Showing user by username: #{params[:username]}"
+    @user = User.find_by username: params[:username]
+    @outfits = Outfit.where userid: @user.id
   end
 end
