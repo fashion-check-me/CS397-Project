cfa15493b3b2109155e8db373b63fee63beba8f2
Joshua Michael Hertlein <jmhertlein@gmail.com>
2015-04-01 11:20:28 -0500

    Using username on outfit page instead of email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    flash.alert = "DEBUG: Showing user by username: #{params[:username]}"
-    @user = User.find_by username: params[:username]
+    @user = User.find_by! username: params[:username]
     @outfits = Outfit.where userid: @user.id
   end
 end
